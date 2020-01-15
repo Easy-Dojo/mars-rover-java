937aca37d40319c3e7da04b15f1066439b6f7c5f
Liang <392425595@qq.com>
2020-01-15 13:53:29 +0800

    连续右转

--- src/test/java/com/thoughtworks/marsrover/MarsRoverTest.java
+++ src/test/java/com/thoughtworks/marsrover/MarsRoverTest.java
@@ -87,4 +87,12 @@ public class MarsRoverTest {
         assertEquals(new MarsStatus(0, 0, Direction.S), marsStatus);
     }
 
+    @Test
+    public void should_return_00S_when_execute_given_init_state_00N_command_RR() {
+        MarsRover marsRover = new MarsRover(0, 0, Direction.N);
+
+        MarsStatus marsStatus = marsRover.execute("RR");
+
+        assertEquals(new MarsStatus(0, 0, Direction.S), marsStatus);
+    }
 }
